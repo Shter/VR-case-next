425a77573202a39725be12075372c5989abba77c
Pavel Aliseika <aliseika.pavel@wisercat.net>
2026-04-11 22:43:02 -0300

    fix build failure

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,21 +3,45 @@ import React from 'react';
 import { TextEncoder, TextDecoder } from 'util';
 import { webcrypto } from 'crypto';
 import { ReadableStream } from 'node:stream/web';
-import { MessageChannel, MessageEvent, MessagePort } from 'worker_threads';
+import { MessageChannel, MessagePort as WorkerMessagePort } from 'worker_threads';
+
+const getMessageEventConstructor = (): typeof globalThis.MessageEvent => {
+  if (typeof MessageEvent !== 'undefined') {
+    return MessageEvent;
+  }
+
+  class MessageEventPolyfill<T = unknown> extends Event {
+    data: T;
+    origin: string;
+    lastEventId: string;
+    source: MessageEventSource | null;
+    ports: MessagePort[];
+
+    constructor(type: string, init?: MessageEventInit<T>) {
+      super(type);
+      this.data = init?.data as T;
+      this.origin = init?.origin ?? '';
+      this.lastEventId = init?.lastEventId ?? '';
+      this.source = init?.source ?? null;
+      this.ports = init?.ports ? [...init.ports] : [];
+    }
+  }
+
+  return MessageEventPolyfill as unknown as typeof globalThis.MessageEvent;
+};
+
+const MessageEventConstructor = getMessageEventConstructor();
 
 
 if (!globalThis.TextEncoder) {
-  // @ts-expect-error assigning for test env
-  globalThis.TextEncoder = TextEncoder;
+  globalThis.TextEncoder = TextEncoder as unknown as typeof globalThis.TextEncoder;
 }
 
 if (!globalThis.TextDecoder) {
-  // @ts-expect-error assigning for test env
-  globalThis.TextDecoder = TextDecoder as typeof globalThis.TextDecoder;
+  globalThis.TextDecoder = TextDecoder as unknown as typeof globalThis.TextDecoder;
 }
 
 if (!globalThis.crypto) {
-  // @ts-expect-error assign webcrypto for tests
   globalThis.crypto = webcrypto as unknown as Crypto;
 }
 
@@ -25,43 +49,35 @@ if (!globalThis.crypto) {
 const { Headers, Request, Response, FormData } = require('undici');
 
 if (!globalThis.Headers) {
-  // @ts-expect-error polyfill
   globalThis.Headers = Headers as typeof globalThis.Headers;
 }
 
 if (!globalThis.Request) {
-  // @ts-expect-error polyfill
   globalThis.Request = Request as typeof globalThis.Request;
 }
 
 if (!globalThis.Response) {
-  // @ts-expect-error polyfill
   globalThis.Response = Response as typeof globalThis.Response;
 }
 
 if (!globalThis.FormData) {
-  // @ts-expect-error polyfill
   globalThis.FormData = FormData as typeof globalThis.FormData;
 }
 
 if (!globalThis.ReadableStream) {
-  // @ts-expect-error polyfill
   globalThis.ReadableStream = ReadableStream as typeof globalThis.ReadableStream;
 }
 
 if (!globalThis.MessageChannel) {
-  // @ts-expect-error polyfill
   globalThis.MessageChannel = MessageChannel as typeof globalThis.MessageChannel;
 }
 
 if (!globalThis.MessagePort) {
-  // @ts-expect-error polyfill
-  globalThis.MessagePort = MessagePort as unknown as typeof globalThis.MessagePort;
+  globalThis.MessagePort = WorkerMessagePort as unknown as typeof globalThis.MessagePort;
 }
 
 if (!globalThis.MessageEvent) {
-  // @ts-expect-error polyfill
-  globalThis.MessageEvent = MessageEvent as unknown as typeof globalThis.MessageEvent;
+  globalThis.MessageEvent = MessageEventConstructor as typeof globalThis.MessageEvent;
 }
 
 process.env.NEXT_PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://vr-case.test';
@@ -73,8 +89,13 @@ process.env.BLOB_MEDIA_PHOTO_PREFIX = process.env.BLOB_MEDIA_PHOTO_PREFIX ?? 'as
 process.env.BLOB_MEDIA_VIDEO_PREFIX = process.env.BLOB_MEDIA_VIDEO_PREFIX ?? 'assets/videos';
 process.env.BLOB_MEDIA_ACCESS = process.env.BLOB_MEDIA_ACCESS ?? 'public';
 
+type NextImageMockProps = React.ComponentProps<'img'> & {
+  priority?: boolean;
+  fill?: boolean;
+};
+
 jest.mock('next/image', () => {
-  return React.forwardRef((props: React.ComponentProps<'img'>, ref: React.Ref<HTMLImageElement>) => {
+  return React.forwardRef((props: NextImageMockProps, ref: React.Ref<HTMLImageElement>) => {
     const { priority, fill, ...rest } = props;
     return React.createElement('img', { ref, ...rest });
   });
@@ -84,8 +105,8 @@ jest.mock('next/link', () => {
   return React.forwardRef((props: { href: any } & React.ComponentProps<'a'>, ref: React.Ref<HTMLAnchorElement>) => {
     const { href, children, ...rest } = props;
     const resolvedHref = typeof href === 'string'
-      ? href
-      : href?.pathname ?? href?.href ?? '#';
+        ? href
+        : href?.pathname ?? href?.href ?? '#';
     return React.createElement('a', { ref, href: resolvedHref, ...rest }, children);
   });
 });
